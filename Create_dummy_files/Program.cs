8a7168dc429e3c774db23bba4e3d59efe1698254
Nerso1 <dominiknowekonto@gmail.com>
2025-01-07 15:48:01 +0100

    Implemented reading data from json, saving changes before implementing interface

--- Create_dummy_files/Program.cs
+++ Create_dummy_files/Program.cs
@@ -9,22 +9,25 @@ using System.Text.Json;
 internal class Program
 {
     public static async Task Main(string[] args)
-    //static async Task Main(string[] args) // Main is now async
     {
         string directoryPath = @"C:\Users\domin\Downloads\Folder";
+        string filePath = "files.json";
         bool genericOrRandom = false;
-        var files = new List<FileCreationRules>
+        var files = await JsonFileHandler.DeserializeFromJsonFileAsync<FileCreationRules>(filePath);
+        foreach (var file in files)
         {
-            new FileCreationRules(10, 1.234, ".rtc", DateTime.Now),
-            new FileCreationRules(5, 0.987, ".log", DateTime.Now.AddDays(-1)),
-            new FileCreationRules(20, 0.123, ".txt", DateTime.Now.AddDays(-2))
-        };
+            Console.WriteLine($"Amount: {file.Amount}, SizeMB: {file.SizeMB}, Extension: {file.Extension}, CreationDate: {file.CreationDate}");
+        }
+
+        //var files = new List<FileCreationRules>
+        //{
+        //    new FileCreationRules(10, 1.234, ".rtc", DateTime.Now),
+        //    new FileCreationRules(5, 0.987, ".log", DateTime.Now.AddDays(-1)),
+        //    new FileCreationRules(20, 0.123, ".txt", DateTime.Now.AddDays(-2))
+        //};
+        //await JsonFileHandler.SerializeToJsonFileAsync(filePath, files);
 
         FileCreationRules.PrintAll(files);
-
-        Console.WriteLine();
-
-        FileCreationRules.CalculateTotalFilesSize(files);
 
         Console.WriteLine();
 
@@ -32,11 +35,52 @@ internal class Program
 
         Console.WriteLine();
 
-        var filesWithNames = await FilePreparer.CombineFileNamesAndData(files, genericOrRandom);
-        FilePreparer.PrintPreparedFiles(filesWithNames);
+        FileCreationRules.CalculateTotalFilesSize(files);
 
         Console.WriteLine();
 
-        FileCreator.CreateFiles(filesWithNames, directoryPath);
+        if (YesOrNoQuestion("Do you want to create files? (y/N) "))
+        {
+            Console.WriteLine();
+
+            var filesWithNames = await FilePreparer.CombineFileNamesAndData(files, genericOrRandom);
+            FilePreparer.PrintPreparedFiles(filesWithNames);
+
+            Console.WriteLine();
+
+            FileCreator.CreateFiles(filesWithNames, directoryPath);
+        }
+        
     }
+
+    public static bool YesOrNoQuestion(string question)
+    {
+        Console.Write(question);
+        ConsoleKeyInfo response = Console.ReadKey();
+        if (response.KeyChar == 'Y' || response.KeyChar == 'y') return true;
+        else return false;
+    }
+}
+
+public static class JsonFileHandler
+{
+    public static async Task SerializeToJsonFileAsync<T>(string filePath, List<T> data)
+    {
+        // Convert the list to a JSON string
+        var options = new JsonSerializerOptions { WriteIndented = true }; // Makes the JSON readable
+        string jsonString = JsonSerializer.Serialize(data, options);
+
+        // Write the JSON string to the file
+        await File.WriteAllTextAsync(filePath, jsonString);
+    }
+
+    public static async Task<List<T>> DeserializeFromJsonFileAsync<T>(string filePath)
+    {
+        // Read the JSON string from the file
+        string jsonString = await File.ReadAllTextAsync(filePath);
+
+        // Deserialize the JSON string into a list
+        return JsonSerializer.Deserialize<List<T>>(jsonString) ?? new List<T>();
+    }
+
 }
